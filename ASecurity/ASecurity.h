99c3546fc02bad6424984c4561978f920e87f1f9
Ihor Myroniuk <ihormyroniuk@gmail.com>
2020-02-20 17:11:08 +0200

    first commit

---
+++ ASecurity/ASecurity.h
@@ -0,0 +1,19 @@
+//
+//  ASecurity.h
+//  ASecurity
+//
+//  Created by Ihor Myroniuk on 2/20/20.
+//  Copyright © 2020 ihormyroniuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ASecurity.
+FOUNDATION_EXPORT double ASecurityVersionNumber;
+
+//! Project version string for ASecurity.
+FOUNDATION_EXPORT const unsigned char ASecurityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASecurity/PublicHeader.h>
+
+
